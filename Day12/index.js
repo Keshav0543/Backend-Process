21e30c34fca4e13a3a534517f6e53998ede6b294
Keshav Thakur <keshavt043@gmail.com>
2026-04-27 19:33:02 +0530

    Update27Apr

--- Day12/index.js
+++ Day12/index.js
@@ -3,9 +3,12 @@ import User from "./models/schema.js";
 import main from "./database.js";
 import ValidInput from "./utils/validator.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 
 app.post("/register", async (req, res) => {
   try {
@@ -22,13 +25,17 @@ app.post("/register", async (req, res) => {
 
 app.post("/login",async (req,res)=>{
   try{
-    const data=await User.findById(req.body._id);
+    const data=await User.findOne({email:req.body.email});
     //Email Checking
     if(req.body.email !== data.email)throw new Error("Invalid Credentials");
 
     //Password Checking
     const IsAllowed=await bcrypt.compare(req.body.password,data.password);
     if(!IsAllowed) throw new Error("Invalid Credentials");
+
+    //jwt 
+    const token=jwt.sign({_id:data._id, email:data.email}, "\x61\x53\x42\x73\x62\x33\x5A\x6C\x49\x48\x55\x67\x63\x48\x4A\x68\x64\x47\x6C\x74\x59\x51\x3D\x3D")
+    res.cookie("token",token);
     
     res.send("Login Successfuly...");   
 
@@ -38,22 +45,27 @@ app.post("/login",async (req,res)=>{
 })
 
 
-app.get("/user", async (req, res) => {
+app.get("/info", async (req, res) => {
   try {
     const data = await User.find({});
+
+    const ans=jwt.verify(req.cookies.token,"\x61\x53\x42\x73\x62\x33\x5A\x6C\x49\x48\x55\x67\x63\x48\x4A\x68\x64\x47\x6C\x74\x59\x51\x3D\x3D");
+
+    console.log(ans);
     res.send(data);
   } catch (err) {
     console.log(err);
   }
 });
 
-app.get("/user/:id", async (req, res) => {
+app.get("/user", async (req, res) => {
   try {
     const id = req.params.id;
-    const info_user = await User.findById(id);
+    const payload=jwt.verify(req.cookies.token,"\x61\x53\x42\x73\x62\x33\x5A\x6C\x49\x48\x55\x67\x63\x48\x4A\x68\x64\x47\x6C\x74\x59\x51\x3D\x3D")
+    const info_user = await User.findById(payload._id);
     res.send(info_user);
   } catch (err) {
-    console.log(err);
+    res.send(err.message);
   }
 });
 
