494f1858eee3cfd5dac18b1667d27e897b5da2a5
Keshav Thakur <keshavt043@gmail.com>
2026-05-11 08:41:33 +0530

    UpdateRatelimitsliding

--- Day12/middleware/rateLimiter.js
+++ Day12/middleware/rateLimiter.js
@@ -1,16 +1,31 @@
 import client from "../config/redis.js";
 
+const Totalttl =600; const maxRequest=60;
+
 async function rateLimiter(req,res,next){
     try{
-        const ip=req.ip;
+        const key=`IP${req.ip}`;
 
-        const count=await client.incr(ip);
+        const currenttime=Date.now()/1000;
+        const window_time=currenttime-Totalttl;
+
+        await client.zRemRangeByScore(key,0,window_time);
+
+        const numberOfreq= await client.zCard(key);
+
+
+
+        if(numberOfreq>maxRequest) throw new Error("Cool down try agaiin after sometime!!!");
+
+        await client.zAdd(key,[{score:currenttime,value:`${currenttime}:${Math.random()}`}]);
         
-        if(count>60)throw new Error("Cool down try again after sometime!!!");
+        //key ke TTl ko increase karna 
+        await client.expire(key,Totalttl);
 
-        if(count==1) await client.expire(ip,3600);
-
+        const ttl = await client.ttl(key);
+        console.log(ttl);
         next();
+
     }
     catch(err){
         res.send(err.message);
